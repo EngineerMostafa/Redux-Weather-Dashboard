84a4f1831124f1e1d743c586284fcc86694c4f83
Mostafa Mahmoud <engineer.mostafa@outlook.com>
2017-11-06 12:03:39 +0200

    Redux Weather Dashboard

---
+++ src/components/chart.js
@@ -0,0 +1,20 @@
+import _ from 'lodash'
+import React from 'react'
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines'
+
+function average(data) {
+  return _.round(_.sum(data)/data.length)
+}
+const Chart = (props) => {
+  return (
+    <div>
+      <Sparklines height={120} width={180} date={props.data}>
+        <SparklinesLines color={props.color}/>
+        <SparklinesReferenceLine type="avg"/>
+      </Sparklines>
+      <div>{average(props.data)} {props.units}</div>
+    </div>
+  )
+}
+
+export default Chart
